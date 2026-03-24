a622f263f0f3eae0fd27af764c55a4664109b9e3
renanhomiak <renanhomiakg@gmail.com>
2026-03-24 15:28:35 -0300

    primeiro commit

---
+++ src/main/java/com/homiak/ex07/models/FornecedorModel.java
@@ -0,0 +1,30 @@
+package com.homiak.ex07.models;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "tb_fornecedor")
+public class FornecedorModel {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "nome_fantasia")
+    private String nomeFantasia;
+
+    @Column(name = "cnpj_empresa")
+    private String cnpj;
+
+    private String contato;
+
+    public Long getId() { return id; }
+
+    public String getNomeFantasia() { return nomeFantasia; }
+    public void setNomeFantasia(String nomeFantasia) { this.nomeFantasia = nomeFantasia; }
+
+    public String getCnpj() { return cnpj; }
+    public void setCnpj(String cnpj) { this.cnpj = cnpj; }
+
+    public String getContato() { return contato; }
+    public void setContato(String contato) { this.contato = contato; }
+}
