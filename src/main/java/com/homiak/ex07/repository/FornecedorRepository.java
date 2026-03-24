d735e1533fada1794c85419060bc6190c08297c4
renanhomiak <renanhomiakg@gmail.com>
2026-03-24 15:30:14 -0300

    segundo commit

---
+++ src/main/java/com/homiak/ex07/repository/FornecedorRepository.java
@@ -0,0 +1,7 @@
+package com.homiak.ex07.repository;
+
+import com.homiak.ex07.models.FornecedorModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface FornecedorRepository extends JpaRepository<FornecedorModel, Long> {
+}
